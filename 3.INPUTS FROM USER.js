35a4249c7c741613d03c6058ebf7e4d1f857d2e7
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 14:33:00 +0530

    Create 3.INPUTS FROM USER.js

---
+++ 3.INPUTS FROM USER.js
@@ -0,0 +1,5 @@
+
+            let a= prompt("enter a number of tickets");
+            console.log(a);
+            console.log(typeof(a));
+            console.log(Number(a)*20 +" rs you should pay");
