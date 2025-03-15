56af2099f0a293f280f7b5f170aed3a40bd164aa
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 12:45:55 +0530

    Create 18.loops.js

---
+++ 18.loops.js
@@ -0,0 +1,16 @@
+let arr=[1,2,3,4,5]
+    for(let fruits of arr){  // simple way of loops
+        console.log(fruits)
+    }
+
+
+    // for in is used to handle objects
+    let hi={
+        name:"jothi",
+        class:"it",
+        year:"iii"
+    }
+    for(let bii in hi){
+        console.log(bii)
+        console.log(hi[bii])
+    }
