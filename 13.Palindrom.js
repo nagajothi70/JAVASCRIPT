ab27f236458c4281e215b942d0fecbfb3a272825
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 12:29:17 +0530

    Create 13.Palindrom.js

---
+++ 13.Palindrom.js
@@ -0,0 +1,10 @@
+
+            let str="happy birthday"
+            let rev=str.reverse;
+           
+            if(str==rev){
+                console.log("palindrom")
+            }
+            else{
+                console.log("not palindrom")
+            }
