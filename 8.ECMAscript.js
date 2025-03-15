13a174668a6da01bd489621d8ee37ffc7e569a1e
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 15:52:33 +0530

    Create 8.ECMAscript.js

---
+++ 8.ECMAscript.js
@@ -0,0 +1,15 @@
+let fname="naga";
+        let lname="jothi";
+        let city="erode";
+        console.log(fname + " " +lname +" "+"lives in "+city);
+    // see the above example for simple statement we have used more than 5 + symbols
+     // to replace this use ecma scripts-----``--backtick
+     let ecmascript=`${fname} ${lname} lives in ${city}`
+     console.log(ecmascript)
+     console.log(`${fname} ${lname}`);
+     // both gives same output 
+     let str= `happy
+birthday`
+     console.log(str)
+     let str1=`"hi she is best girls her's fater has expensive bike"`
+     console.log(str1)
