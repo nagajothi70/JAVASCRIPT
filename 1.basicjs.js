5cf73c73c2b7f4d7ec1e3af623bca38669a2f19c
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 12:11:46 +0530

    Create 1.basicjs.js

---
+++ 1.basicjs.js
@@ -0,0 +1,39 @@
+// this is single comment line in js 
+        /*
+        this is multi line comment in js 
+        */
+        console.log("lets  start js journey");  
+        // this console.log is used as print statement in js but prints only in console
+        let price=0;
+        price=1000;
+        
+        // like this it can be changed dynamically
+        console.log("price of dress is:"+price);
+        let one=document.getElementById("one");
+one.innerHTML="price of dress is :"+price;
+const pi=3.14;
+       // pi=78;// this makes error because const variable connaot be changed
+        // this var type also can be used 
+        // js is also called dynamically types language because variable type can be changed freely
+        var a = 1;
+        a="hi";
+        a=1.9;
+        console.log(a)
+        // variables  can also use $ , _ symbols
+         let $="jothi";
+         let _="naga";
+         console.log(`${$} ${_}`);
+         // datatypes 8 primitive datatypes
+        //  1.Number
+        //  2.string
+        //  3.symbol
+        //  4.indefined
+        //  5.null
+        //  6.boolean
+        //  7.bigint
+        //  8.objects
+
+        // any big number beyond the datatype number range then bigint can be used 
+        let bigint=88973907007036973697n;
+        console.log(bigint);
+        // small n at last of number range denotes it as bigint
