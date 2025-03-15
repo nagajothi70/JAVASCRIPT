10f5d4e366fa2c6c9c5faf3309935007d2c7da60
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 14:47:53 +0530

    Create 4.DOM.js

---
+++ 4.DOM.js
@@ -0,0 +1,24 @@
+var hi=document.getElementById("hoi")
+hi.innerHTML="hi everyone"
+</script>
+
+
+<hr>
+
+<h1> this is DOM using  class</h1>
+<div class="one"> </div>
+<script >
+var hi = document.getElementsByClassName("one")
+hi[0].innerHTML="hi hello"
+</script> 
+
+<hr>
+
+<h1> this is DOM using tag name </h1>
+<p>hi </p>
+<p>guys</p>
+<br>
+<script>
+var hi=document.getElementsByTagName("p");
+hi[0].innerHTML="hii i am nagajothi"
+</script>
