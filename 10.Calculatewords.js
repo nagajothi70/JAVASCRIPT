20fced4a5e6aa8139f21a19a89030163e7a6ee29
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 12:16:49 +0530

    Create 10.Calculatewords.js

---
+++ 10.Calculatewords.js
@@ -0,0 +1,7 @@
+ function calculate(){
+        let str=document.getElementById("two").value;
+        let arr=str.split(" ");
+        let num=arr.length;
+        let result=document.getElementById("four");
+        result.innerHTML=num;
+        }
