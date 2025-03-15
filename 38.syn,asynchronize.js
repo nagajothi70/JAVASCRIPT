089625f8ce308fcdb960d3b0b31e73a9c007c6b7
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 14:42:38 +0530

    Create 38.syn,asynchronize.js

---
+++ 38.syn,asynchronize.js
@@ -0,0 +1,19 @@
+console.log("step1")
+            console.log("step2");
+            console.log("step3")
+            console.log("step4");
+            let a=2;
+            console.log(a)
+
+
+            setTimeout(()=>{console.log("step1")},4000)
+            setTimeout(()=>{console.log("step2")},3000)
+
+            setTimeout(()=>{console.log("step3")},2000)
+for(let i=0;i<10;i++){
+    console.log(i)
+}
+
+// timeinterval
+setInterval(()=>{
+    console.log("hi")},2000)
