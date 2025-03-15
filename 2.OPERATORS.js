72223c8ed9f8c784dd26bb3a4585842d3d86e949
NAGAJOTHI. T.R <nagajothierode7@gmail.com>
2025-03-15 14:02:09 +0530

    Create 2.OPERATORS.js

---
+++ 2.OPERATORS.js
@@ -0,0 +1,16 @@
+
+                let  a="8"
+                console.log(typeof(a));
+let b=Number(a)
+               console.log(Number(a));
+                console.log(typeof(b));
+                // type conversions
+                // 1.String(a);
+                // 2.number(a);
+                // 3.Boolean(a);
+                let gh= 3;
+                console.log(String(gh));
+                console.log(Number(gh));
+                console.log(Boolean(gh));
+
+            
